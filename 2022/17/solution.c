8ba9699b9790e5a30a478e02726d05c787fe7cce
kowasaur <75297283+kowasaur@users.noreply.github.com>
2022-12-18 13:36:11 +1000

    2022 day 17 part 2
    I also struggled with this. I knew it was cyclic but I didn't know how to find a cycle. A comment on reddit explained how to find it

--- 2022/17/solution.c
+++ 2022/17/solution.c
@@ -1,3 +1,5 @@
+// For part 2 I got help from https://www.reddit.com/r/adventofcode/comments/zo27vf/2022_day_17_part_2_rocks_fall_nobody_dies/
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -5,6 +7,13 @@
 
 #define CHAMBER_WIDTH 7
 #define CHAMBER_HEIGHT 10000
+
+typedef struct CycleInfo {
+    int rock_index;
+    int height;
+} CycleInfo;
+
+typedef long long ll;
 
 const bool rocks[5][4][4] = {
     {{true, true, true, true}},
@@ -44,57 +53,45 @@ bool doesCollide(int r, int bl_x, int bl_y) {
     return false;
 }
 
-void printChamberWithNew(int r, int bl_y, int bl_x) {
-    printf("-------\n");
-    for (int y = 14; y >= 1; y--) {
-        for (int x = 0; x < CHAMBER_WIDTH; x++) {
-            char c = chamber[y][x] ? '#' : '.';
-            if (bl_x <= x && x < bl_x + rock_width[r] && bl_y <= y && y < bl_y + rock_height[r]) {
-                if (rocks[r][y - bl_y][x - bl_x]) c = '@';
-            }
-            putc(c, stdout);
-        }
-        putc('\n', stdout);
+void resetChamber() {
+    for (int y = 1; y < CHAMBER_HEIGHT; y++) {
+        for (int x = 0; x < CHAMBER_WIDTH; x++) chamber[y][x] = false;
     }
-    printf("-------\n\n");
+    for (int x = 0; x < CHAMBER_WIDTH; x++) chamber[0][x] = true;
 }
 
-void printChamber() {
-    printf("-------\n");
-    for (int y = 20; y >= 1; y--) {
-        for (int x = 0; x < CHAMBER_WIDTH; x++) {
-            putc(chamber[y][x] ? '#' : '.', stdout);
-        }
-        putc('\n', stdout);
-    }
-    printf("=======\n\n\n");
-}
-
-int main() {
-    FILE *file = fopen("input.txt", "r");
-    fseek(file, 0, SEEK_END);
-    long file_size = ftell(file);
-    fseek(file, 0, SEEK_SET);
-
-    bool *move_left;
-    move_left = malloc(file_size);
-    int n = 0;
-    char c;
-    while ((c = fgetc(file)) != EOF) move_left[n++] = c == '<';
-
+ll rockTowerHeight(ll rocks_count, bool *move_left, long file_size) {
     int height = 1; // have to account for this later
     int i = -1;
 
-    for (int rock_index = 0; rock_index < 2022; rock_index++) {
+    CycleInfo cycle_info[5][file_size];
+    for (int r = 0; r < 5; r++) {
+        for (int c = 0; c < file_size; c++) cycle_info[r][c].height = 0;
+    }
+
+    for (int rock_index = 0; rock_index < rocks_count; rock_index++) {
         const int r = rock_index % 5;
+        const int next_i = (i + 1) % file_size;
+
+        if (cycle_info[r][next_i].height) {
+            int h = cycle_info[r][next_i].height ;
+            int ri = cycle_info[r][next_i].rock_index;
+            if ((rocks_count - ri) % (rock_index - ri) == 0) {
+                resetChamber();
+                return h - 1 + (height - h) * (rocks_count - ri) / (rock_index - ri);
+            }
+        }
+
+        cycle_info[r][next_i].rock_index = rock_index;
+        cycle_info[r][next_i].height = height;
 
         // bottom left of rock coordinates
         int bl_x = 2;
         int bl_y = height + 3;
 
         while (true) {
-            i++;
-            int new_bl_x = move_left[i % file_size] ? bl_x - 1 : bl_x + 1;
+            i = (i + 1) % file_size;
+            int new_bl_x = move_left[i] ? bl_x - 1 : bl_x + 1;
             int new_bl_y = bl_y - 1; 
 
             if (new_bl_x >= 0 && rock_width[r] + new_bl_x <= CHAMBER_WIDTH && !doesCollide(r, new_bl_x, bl_y)) {
@@ -114,7 +111,24 @@ int main() {
         height = MAX(bl_y + rock_height[r], height);
     }
 
-    printf("Part 1: %d\n", height - 1);
+    resetChamber();
+    return height - 1;
+}
+
+int main() {
+    FILE *file = fopen("input.txt", "r");
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    bool *move_left;
+    move_left = malloc(file_size);
+    int n = 0;
+    char c;
+    while ((c = fgetc(file)) != EOF) move_left[n++] = c == '<';
+
+    printf("Part 1: %lld\n", rockTowerHeight(2022, move_left, file_size));
+    printf("Part 2: %lld\n", rockTowerHeight(1000000000000, move_left, file_size));
 
     return 0;
 }
