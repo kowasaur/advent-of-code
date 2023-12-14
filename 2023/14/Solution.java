b2c2886574b3e75033a7244b4f8173b3f4e49631
kowasaur <75297283+kowasaur@users.noreply.github.com>
2023-12-14 22:26:03 +1000

    2023 day 14 part 2

--- 2023/14/Solution.java
+++ 2023/14/Solution.java
@@ -35,19 +35,36 @@ class Solution {
     }
 
     public static void main(String[] _args) throws IOException {
-        var grid = AoC.readGrid("14/sample.txt");
+        var grid = AoC.readGrid("14/input.txt");
 
         var grid1 = grid.copy();
         tilt(grid1);
         AoC.printResult(1, load(grid1));
 
-        Grid old_grid;
-        for (int i = 0; i < 1000000000; i++) {
-            old_grid = grid;
+        var loads = new HashMap<Integer, ArrayList<Integer>>();
+        for (int i = 0; i < 100; i++) grid = cycle(grid);
+        for (int i = 100; i < 150; i++) {
             grid = cycle(grid);
-            if (grid.equals(old_grid)) break;
+            var l = load(grid);
+            if (loads.containsKey(l)) loads.get(l).add(i);
+            else {
+                var indices = new ArrayList<Integer>();
+                indices.add(i);
+                loads.put(l, indices);
+            }
         }
 
-        AoC.printResult(2, load(grid));
+        int modulo = -1;
+        for (var entry : loads.entrySet()) {
+            var inds = entry.getValue();
+            if (inds.size() == 3) modulo = inds.get(2) - inds.get(0);
+        }
+
+        for (var entry : loads.entrySet()) {
+            if ((999999999 - entry.getValue().getLast()) % modulo == 0) {
+                AoC.printResult(2, entry.getKey());
+                return;
+            }
+        }
     }
 }
