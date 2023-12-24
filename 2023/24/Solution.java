c34c9297fa0f34c51f3a5cf8c1ef3ba10bff6e2a
kowasaur <75297283+kowasaur@users.noreply.github.com>
2023-12-24 17:43:44 +1000

    2023 day 24 part 2

--- 2023/24/Solution.java
+++ 2023/24/Solution.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import java.util.*;
 import utils.*;
 
 class Solution {
@@ -14,9 +13,18 @@ class Solution {
         }
         AoC.printResult(1, result1);
 
-        int result2 = 0;
-
-        AoC.printResult(2, result2);
+        System.out.println("For Part 2, run the following in Mathematica: ");
+        System.out.print("sol = Solve[");
+        for (int i = 0; i < 3; i++) {
+            var h = hailstones.get(i);
+            char c = (char)('m'+ i);
+            System.out.print("x+a*" + c +"==" + h.pos[0]+"+(" +h.vel[0]+")*"+ c +"&&");
+            System.out.print("y+b*" + c +"==" + h.pos[1]+"+(" +h.vel[1]+")*"+ c +"&&");
+            System.out.print("z+c*" + c +"==" + h.pos[2]+"+(" +h.vel[2]+")*"+ c);
+            if (i != 2) System.out.print("&&");
+        }
+        System.out.println(", {x,y,z,a,b,c,m,n,o}]");
+        System.out.println("x + y + z /. sol[[1]]");
     }
 }
 
@@ -37,19 +45,12 @@ class Hailstone {
         var m1 = (double)vel[1] / vel[0];
         var m2 = (double)other.vel[1] / other.vel[0];
         if (m1 == m2) return false;
-        // if (other.vel[1] * vel[0] == vel[1] * other.vel[0]) return false;
-        // var t1 = intersectTime(other, 0);
-        // var t2 = intersectTime(other, 1);
+
         var x = (other.pos[1] - pos[1] + m1*pos[0] - m2*other.pos[0]) / (m1 - m2);
         var y = m1 * (x - pos[0]) + pos[1];
 
         if (x < MIN || y < MIN || x > MAX || y > MAX) return false;
-
         return !(inPast(x) || other.inPast(x));
-    }
-
-    double intersectTime(Hailstone other, int i) {
-        return (pos[i] - other.pos[i]) / (other.vel[i] - vel[i]);
     }
 
     boolean inPast(double x) {
