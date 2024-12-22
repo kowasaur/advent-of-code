18ae0f9e8ddc81bb4adfa301b44a5d740fed4345
kowasaur <75297283+kowasaur@users.noreply.github.com>
2024-12-22 14:50:37 +1000

    2024 day 21 give up on part 2

--- 2024/21/solution.py
+++ 2024/21/solution.py
@@ -13,9 +13,9 @@ def possibilities(fro: str, to: str, pad: str) -> list[str]:
     jx, jy = j % 3, j // 3
     dx, dy = jx - x, jy - y
     if dx == 0:
-        return ["^" * dy if dy > 0 else "v" * -dy]
+        return ["^" * dy + "A" if dy > 0 else "v" * -dy + "A"]
     if dy == 0:
-        return [">" * dx if dx > 0 else "<" * -dx]
+        return [">" * dx + "A" if dx > 0 else "<" * -dx + "A"]
 
     space_row = 0 if pad[0] == " " else 1
 
@@ -23,14 +23,9 @@ def possibilities(fro: str, to: str, pad: str) -> list[str]:
     yc = "^" * dy if dy > 0 else "v" * -dy
 
     if x != 0 or jy != space_row:
-        result.append(yc + xc)
+        result.append(yc + xc + "A")
     if y != space_row or jx != 0:
-        result.append(xc + yc)
-
-    if len(result) == 0:
-        print(fro)
-        print(to)
-        print(pad)
+        result.append(xc + yc + "A")
 
     return result
 
@@ -38,26 +33,31 @@ def possibilities(fro: str, to: str, pad: str) -> list[str]:
 def moves(goals: list[str], pad: str) -> list[str]:
     fr_result = []
     for goal in goals:
-        result = [""]
+        result: list[list[str]] = []
         i = pad.index("A")
 
         for c in goal:
-            new_result = []
-            for poss in possibilities(pad[i], c, pad):
-                for r in result:
-                    new_result.append(r + poss + "A")
-            result = new_result
+            result.append(possibilities(pad[i], c, pad))
+            # new_result = []
+            # for poss in possibilities(pad[i], c, pad):
+            #     for r in result:
+            #         new_result.append(r + poss)
+            # result = new_result
             i = pad.index(c)
         fr_result.extend(result)
 
     return fr_result
 
 
-def func(line: str) -> int:
-    first = moves([line], NUMPAD)
-    second = moves(first, DIRPAD)
-    third = moves(second, DIRPAD)
-    return int(line[:-1]) * min(len(x) for x in third)
+def func(line: str, robots: int) -> int:
+    result = moves([line], NUMPAD)
+    min_len = 0
+    for _ in range(robots):
+        result = moves(result, DIRPAD)
+        min_len = min(len(x) for x in result)
+        result = [x for x in result if len(x) == min_len]
+
+    return int(line[:-1]) * min_len
 
 
 def main():
@@ -68,7 +68,8 @@ def main():
     p2 = 0
 
     for line in lines:
-        p1 += func(line)
+        p1 += func(line, 2)
+        p2 += func(line, 2)
 
     print("Part 1:", p1)
 
